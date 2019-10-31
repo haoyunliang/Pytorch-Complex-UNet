4aaeac6ab765a9fad517f9b481588f4337649e56
Hw Leung <441460766@qq.com>
2019-10-31 16:35:39 +0800

    Update unet_1.py

--- unet/unet_1.py
+++ unet/unet_1.py
@@ -50,17 +50,17 @@ class ComplexUnet(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(ComplexUnet, self).__init__()
 
-        self.conv1 = ComplexConv2d(in_channel=in_channels, out_channel=64, kernel_size=3, stride=2)                 # 127, 64
-        self.conv2 = ComplexConv2d(in_channel=64, out_channel=128, kernel_size=3, stride=2)                         # 63 , 128
-        self.conv3 = ComplexConv2d(in_channel=128, out_channel=256, kernel_size=3, stride=2)                        # 31 , 256
-        self.conv4 = ComplexConv2d(in_channel=256, out_channel=512, kernel_size=3, stride=2)                        # 15 , 512
-        self.conv5 = ComplexConv2d(in_channel=512, out_channel=1024, kernel_size=3, stride=2)                       # 7  , 1024
+        self.conv1 = ComplexConv2d(in_channel=in_channels, out_channel=64, kernel_size=3, stride=2)                 
+        self.conv2 = ComplexConv2d(in_channel=64, out_channel=128, kernel_size=3, stride=2)                       
+        self.conv3 = ComplexConv2d(in_channel=128, out_channel=256, kernel_size=3, stride=2)          
+        self.conv4 = ComplexConv2d(in_channel=256, out_channel=512, kernel_size=3, stride=2)                       
+        self.conv5 = ComplexConv2d(in_channel=512, out_channel=1024, kernel_size=3, stride=2)                    
 
-        self.deconv1 = ComplexConvTranspose2d(in_channel=1024, out_channel=512, kernel_size=3, stride=2)            # 15, 512
-        self.deconv2 = ComplexConvTranspose2d(in_channel=512, out_channel=256, kernel_size=3, stride=2)             # 31, 256
-        self.deconv3 = ComplexConvTranspose2d(in_channel=256, out_channel=128, kernel_size=3, stride=2)             # 63 , 128
-        self.deconv4 = ComplexConvTranspose2d(in_channel=128, out_channel=64, kernel_size=3, stride=2)              # 127, 64
-        self.deconv5 = ComplexConvTranspose2d(in_channel=64, out_channel=out_channels, kernel_size=3, stride=2, output_padding=1)     # 255, 12
+        self.deconv1 = ComplexConvTranspose2d(in_channel=1024, out_channel=512, kernel_size=3, stride=2)          
+        self.deconv2 = ComplexConvTranspose2d(in_channel=512, out_channel=256, kernel_size=3, stride=2)            
+        self.deconv3 = ComplexConvTranspose2d(in_channel=256, out_channel=128, kernel_size=3, stride=2)          
+        self.deconv4 = ComplexConvTranspose2d(in_channel=128, out_channel=64, kernel_size=3, stride=2)             
+        self.deconv5 = ComplexConvTranspose2d(in_channel=64, out_channel=out_channels, kernel_size=3, stride=2, output_padding=1)  
 
         self.relu = nn.ReLU()
         self.dc = DataConsistency()
